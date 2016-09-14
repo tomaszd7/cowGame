a8d4808b737b438f8b7d0db3c6c4956786a115b3
Tomasz Dąbrowski <tdabro@wp.pl>
2016-09-14 15:21:54 +0200

    CowGame ver. 0.1 added random movement of chars

--- js/Character.js
+++ js/Character.js
@@ -5,21 +5,62 @@ function Character(type, x, y) {
     this.WIDTH = 20;
     this.HEIGHT = 20;
     this.BORDER = 1;
+    
+    this.SIZE = this.WIDTH + 2 * this.BORDER;
 
     this.type = type;
+    this.state = null;
     this.posX = x;
     this.posY = y;
 
     this.box = null;
+    
+    this.moveLeft = {
+        'left': -this.SIZE,
+        'top': 0
+    }
 
+    
+    this.moves = [-1, 0, 1];
+    
+    this.getRandomVal = function () {
+//        console.log(this.getRandomVal());
+        return this.moves[Math.floor(Math.random()* this.moves.length)];
+    }
+    
+    this.randomMoves = function () {
+        return {
+          'left': this.getRandomVal() * this.SIZE,  
+          'top': this.getRandomVal() * this.SIZE  
+        };
+    }
+
+    this.currentAction = this.randomMoves();
+    
     this.attrs = {
         height: this.HEIGHT + 'px',
         width: this.WIDTH + 'px',
         position: 'absolute',
-        background: '#CCC',
+        background: '#4695e8',        
         top: (this.posY  * (this.HEIGHT + this.BORDER * 2) + this.BORDER) + 'px',
         left:(this.posX * (this.WIDTH + this.BORDER * 2) + this.BORDER) + 'px',
         margin: '0' // add 1 px         
+    }
+
+    this.doAction = function() {
+//        console.log('action');
+//        console.log(this, parseInt(this.attrs['left']), parseInt(this.attrs['top']));
+        this.currentAction = this.randomMoves();
+//        console.log(this.currentAction);
+        var newLeft = parseInt(this.attrs['left']) + this.currentAction['left'];
+        var newTop = parseInt(this.attrs['top']) + this.currentAction['top'];
+        
+        if ( newLeft > 0 && newLeft < this.SIZE * 10 && newTop > 0 && newTop < this.SIZE * 10) {
+            this.attrs['left'] = newLeft + 'px';
+            this.attrs['top'] = newTop + 'px';
+            $(this.box).stop().animate({left: this.attrs['left'], top: this.attrs['top']}, 250, 'linear')            
+        }
+        
     }
 
     this.createStyleAttr = function (attrs) {
@@ -32,6 +73,8 @@ function Character(type, x, y) {
 
     this.createBox = function () {
         this.box = document.createElement('div');
+        // add border radius
+        this.attrs['border-radius'] = '15px';
         this.box.setAttribute('style', this.createStyleAttr(this.attrs));
     }
 
--- js/CowBoard.js
+++ js/CowBoard.js
@@ -3,11 +3,16 @@ function CowBoard() {
     
     this.WIDTH = 10;
     this.HEIGHT = 10;
-    this.SIZE = 22;
+    this.INNER_BORDER = 1;
+    this.SIZE = 20 + 2 * this.INNER_BORDER;
     this.board = [];
     this.charactersOnBoard = [];
     
     this.htmlNode = null;
+    
+//    this.intervalFunc = null;
+    
+
     
     this.boardAttrs = {
         height: (this.SIZE * this.HEIGHT) + 'px',
@@ -29,25 +34,26 @@ function CowBoard() {
     }
     
     this.initiateCharaters = function() {
-        var char1 = new Character('cow', 3, 2)
-        this.charactersOnBoard.push(char1);
-        
+        for (var i = 0; i < 80; i++) {
+            var char1 = new Character('cow', 5, 5)
+            this.charactersOnBoard.push(char1);
+            
+        }
+
     }
     
     this.createHtmlNode = function() {
         var body = document.getElementsByTagName('body');
         this.htmlNode = document.createElement('div');
         this.htmlNode.setAttribute('style', this.createStyleAttr(this.boardAttrs));
+        this.htmlNode.intervalFunc = null;
         body[0].appendChild(this.htmlNode);        
     }
     
     this.displayBoard = function() {
-        console.log(this.htmlNode);
         for (var i = 0; i < this.charactersOnBoard.length; i++) {            
             var char = this.charactersOnBoard[i].getBox();
-            console.log(this.charactersOnBoard[i]);
             this.htmlNode.appendChild(char);
-            console.log(this.htmlNode);
         }        
         
     }
@@ -60,11 +66,30 @@ function CowBoard() {
         return result;
     }    
     
+    this.runGameLoop = function () {
+        console.log('loop');
+        console.log(this.charactersOnBoard);
+        for (var i = 0; i < this.charactersOnBoard; i++) {
+            this.charactersOnBoard[i].doAction();
+        }
+    }
+    
+    this.stopGameLoopListner = function() {
+        // adding listener to htmlNode - board 
+        this.htmlNode.addEventListener('click', function(event) {
+            console.log('ask stop');
+//            console.log(event);
+            clearInterval(event.target.intervalFunc);
+        })
+    }
+    
     this.main = function() {
         this.generateBoard();
         this.initiateCharaters();
         this.createHtmlNode();
         this.displayBoard();
+//        this.htmlNode.intervalFunc = setInterval(this.runGameLoop, 500);
+//        this.stopGameLoopListner();
     }
     
     this.main();
