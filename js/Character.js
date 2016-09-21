9a2b56fb6c78ec3013c1d713843e111226004cbb
Tomasz Dąbrowski <tdabro@wp.pl>
2016-09-21 10:15:13 +0200

    Added user figure

--- js/Character.js
+++ js/Character.js
@@ -2,8 +2,10 @@
 function Character(type, x, y) {
     // drawing by absolute position !!!
 
-    this.WIDTH = 20;
-    this.HEIGHT = 20;
+    this.BOARD = 10;
+
+    this.WIDTH = 60;
+    this.HEIGHT = 60;
     this.BORDER = 1;
     
     this.SIZE = this.WIDTH + 2 * this.BORDER;
@@ -28,6 +30,14 @@ function Character(type, x, y) {
         return this.moves[Math.floor(Math.random()* this.moves.length)];
     }
     
+    
+    this.prepareMoves = function (x, y) {
+        return {
+          'left': x * this.SIZE,  
+          'top': y * this.SIZE  
+        };
+    }
+
     this.randomMoves = function () {
         return {
           'left': this.getRandomVal() * this.SIZE,  
@@ -41,26 +51,57 @@ function Character(type, x, y) {
         height: this.HEIGHT + 'px',
         width: this.WIDTH + 'px',
         position: 'absolute',
-        background: '#4695e8',        
+//        background: '#4695e8',        
         top: (this.posY  * (this.HEIGHT + this.BORDER * 2) + this.BORDER) + 'px',
         left:(this.posX * (this.WIDTH + this.BORDER * 2) + this.BORDER) + 'px',
         margin: '0' // add 1 px         
     }
 
-    this.doAction = function() {
-//        console.log('action');
-//        console.log(this, parseInt(this.attrs['left']), parseInt(this.attrs['top']));
-        this.currentAction = this.randomMoves();
-//        console.log(this.currentAction);
-        var newLeft = parseInt(this.attrs['left']) + this.currentAction['left'];
-        var newTop = parseInt(this.attrs['top']) + this.currentAction['top'];
-        
-        if ( newLeft > 0 && newLeft < this.SIZE * 10 && newTop > 0 && newTop < this.SIZE * 10) {
-            this.attrs['left'] = newLeft + 'px';
-            this.attrs['top'] = newTop + 'px';
-            $(this.box).stop().animate({left: this.attrs['left'], top: this.attrs['top']}, 250, 'linear')            
+    this.isFieldEmpty = function(board, x, y) {
+        if (x >=0 && y >= 0 & x < this.BOARD & y < this.BOARD) {
+            if (board[y][x]['unit'] === null) {
+                return true;
+            }            
         }
+        return false;
+    }
+    
+    this.getRandoms = function() {
+        return {
+            x: this.getRandomVal(),
+            y: this.getRandomVal()
+        }
+    }
+    
+    this.doAction = function(board) {
+        console.log('action');
+        var randoms = this.getRandoms();
+        // get radom position 
+        var newX = this.posX + randoms['x'];
+        var newY = this.posY + randoms['y'];
+
+        // check if no cow on that place 
         
+        if (this.isFieldEmpty(board, newX, newY)) {
+            
+            this.currentAction = this.prepareMoves(randoms['x'], randoms['y']);
+    //        console.log(this.currentAction);
+            var newLeft = parseInt(this.attrs['left']) + this.currentAction['left'];
+            var newTop = parseInt(this.attrs['top']) + this.currentAction['top'];
+
+            if ( newLeft > 0 && newLeft < this.SIZE * this.BOARD && newTop > 0 && newTop < this.SIZE * this.BOARD) {
+                this.attrs['left'] = newLeft + 'px';
+                this.attrs['top'] = newTop + 'px';
+                // add on board
+                board[this.posY][this.posX]['unit'] = null;
+                board[newY][newX]['unit'] = 'cow';
+                this.posX = newX;
+                this.posY = newY;   
+//                console.log(board);
+                
+                $(this.box).stop().animate({left: this.attrs['left'], top: this.attrs['top']}, 1000, 'linear')            
+            }
+        }                        
     }
 
     this.createStyleAttr = function (attrs) {
@@ -74,8 +115,9 @@ function Character(type, x, y) {
     this.createBox = function () {
         this.box = document.createElement('div');
         // add border radius
-        this.attrs['border-radius'] = '15px';
+//        this.attrs['border-radius'] = '15px';
         this.box.setAttribute('style', this.createStyleAttr(this.attrs));
+        $(this.box).append('<img src="cow.png" style="width:'+this.WIDTH+'px;height:'+this.HEIGHT+'px" />');
     }
 
     this.getBox = function () {
--- js/CowBoard.js
+++ js/CowBoard.js
@@ -4,9 +4,10 @@ function CowBoard() {
     this.WIDTH = 10;
     this.HEIGHT = 10;
     this.INNER_BORDER = 1;
-    this.SIZE = 20 + 2 * this.INNER_BORDER;
+    this.SIZE = 60 + 2 * this.INNER_BORDER;
     this.board = [];
     this.charactersOnBoard = [];
+    this.user = {};
     
     this.htmlNode = null;
     
@@ -18,28 +19,108 @@ function CowBoard() {
         height: (this.SIZE * this.HEIGHT) + 'px',
         width: (this.SIZE * this.WIDTH) + 'px',
         position: 'relative',
-        background: '#EEE',
+        background: '#21c014',
         border: '1px solid black',
         margin: 'auto' // add 1 px          
     }
+    
+//    this.boardCell = {
+//        'surface': null,
+//        'unit': null
+//    }
     
     this.generateBoard = function() {
         for (var y = 0; y < this.HEIGHT; y++) {
             var row = [];
             for (var x = 0; x < this.WIDTH; x++) {
-                row[x] = null;
+//                row[x] = this.boardCell; // tak w klasie uzywam zawsze tego samego pola !!!
+                   row[x] = {
+                        'surface': null,
+                        'unit': null
+                    }
             }
             this.board.push(row);
         }        
     }
     
     this.initiateCharaters = function() {
-        for (var i = 0; i < 80; i++) {
+        for (var i = 0; i < 1; i++) {
             var char1 = new Character('cow', 5, 5)
-            this.charactersOnBoard.push(char1);
-            
+            this.charactersOnBoard.push(char1);            
+            this.addCowToBoard(char1.posX, char1.posY);            
+            var char1 = new Character('cow', 8, 1)
+            this.charactersOnBoard.push(char1);            
+            this.addCowToBoard(char1.posX, char1.posY);            
+            var char1 = new Character('cow', 2, 5)
+            this.charactersOnBoard.push(char1);            
+            this.addCowToBoard(char1.posX, char1.posY);            
+            var char1 = new Character('cow', 2, 2)
+            this.charactersOnBoard.push(char1);            
+            this.addCowToBoard(char1.posX, char1.posY);            
+            var char1 = new Character('cow', 7, 5)
+            this.charactersOnBoard.push(char1);            
+            this.addCowToBoard(char1.posX, char1.posY);            
+            var char2 = new Character('cow', 3, 3)
+            this.charactersOnBoard.push(char2);            
+            this.addCowToBoard(char2.posX, char2.posY);            
         }
+        
+        // create user !!!
+        
+        this.user = new Character('user', 1, 1);
+        
+        // change object 
+        this.user.createBox = function () {
+            this.box = document.createElement('div');
+            this.box.setAttribute('style', this.createStyleAttr(this.attrs));
+            $(this.box).append('<img src="user.png" style="width:'+this.WIDTH+'px;height:'+this.HEIGHT+'px" />');
+        }        
+        
+        this.user.doAction = function(board, move) {
+            console.log('user action');
+//            console.log(board);
+            var newX = this.posX + move['x'];
+            var newY = this.posY + move['y'];
 
+            // check if no cow on that place 
+
+            if (this.isFieldEmpty(board, newX, newY)) {
+
+                this.currentAction = this.prepareMoves(move['x'], move['y']);
+        //        console.log(this.currentAction);
+                var newLeft = parseInt(this.attrs['left']) + this.currentAction['left'];
+                var newTop = parseInt(this.attrs['top']) + this.currentAction['top'];
+
+                if ( newLeft > 0 && newLeft < this.SIZE * 10 && newTop > 0 && newTop < this.SIZE * 10) {
+                    this.attrs['left'] = newLeft + 'px';
+                    this.attrs['top'] = newTop + 'px';
+                    // add on board
+                    board[this.posY][this.posX]['unit'] = null;
+                    board[newY][newX]['unit'] = 'cow';
+                    this.posX = newX;
+                    this.posY = newY;   
+    //                console.log(board);
+
+                    $(this.box).stop().animate({left: this.attrs['left'], top: this.attrs['top']}, 500, 'linear')            
+                }
+            }                        
+        }        
+        
+        this.user.createBox();        
+        this.addUserToBoard(this.user.posX, this.user.posY);  
+        
+    }
+    
+    this.addCowToBoard = function(posX, posY) {
+//        this.boardCell['unit'] = 'cow'; // to jest referencja !!!!
+        this.board[posY][posX] = {surface: null,
+                                unit: 'cow'};
+    }
+
+    this.addUserToBoard = function(posX, posY) {
+        this.board[posY][posX] = {surface: null,
+                                unit: 'user'};
+        console.log('user added');
     }
     
     this.createHtmlNode = function() {
@@ -56,6 +137,9 @@ function CowBoard() {
             this.htmlNode.appendChild(char);
         }        
         
+        // display user 
+        this.htmlNode.appendChild(this.user.getBox());
+        
     }
     
     this.createStyleAttr = function (attrs) {
@@ -67,25 +151,32 @@ function CowBoard() {
     }    
     
     this.runGameLoop = function () {
+        // bo to zadziala z punktu przegladarki dopiero a nie klasy teraz tu !!!!
+        // bo ten kod sie wykonuje w przegladarce a nie na serwerze 
         console.log('loop');
-        console.log(this.charactersOnBoard);
-        for (var i = 0; i < this.charactersOnBoard; i++) {
-            this.charactersOnBoard[i].doAction();
+//        console.log(this);
+        for (var i = 0; i < this.charactersOnBoard.length; i++) {
+//            console.log(this.charactersOnBoard[i]);
+            this.charactersOnBoard[i].doAction(this.board);
         }
+//        console.log(this.charactersOnBoard);
+//        console.log(this.board);
     }
     
-    this.stopGameLoopListner = function() {
-        // adding listener to htmlNode - board 
-        this.htmlNode.addEventListener('click', function(event) {
-            console.log('ask stop');
-//            console.log(event);
-            clearInterval(event.target.intervalFunc);
-        })
-    }
+//    this.stopGameLoopListner = function() {
+//        // adding listener to htmlNode - board 
+//        this.htmlNode.addEventListener('click', function(event) {
+//            console.log('ask stop');
+//            console.log(event.target);
+//            clearInterval(event.target.intervalFunc);
+//        })
+//    }
     
     this.main = function() {
         this.generateBoard();
+        console.log(this.board);
         this.initiateCharaters();
+        console.log(this.board);
         this.createHtmlNode();
         this.displayBoard();
 //        this.htmlNode.intervalFunc = setInterval(this.runGameLoop, 500);
